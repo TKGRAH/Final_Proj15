f35a9d09af89fdfbd4e922618e65055fd32aeb9e
Thomas Graham <tkgrah6771@ung.edu>
2015-11-28 22:30:06 -0500

    Syntax int/list error fix
    Fixed type error for logic lines, was missing min() function for second variables.

--- finalproject.py
+++ finalproject.py
@@ -7,14 +7,17 @@ def numdiceroll(x, y):
     if attacking >= 3 and defending >= 2:
         dicea = random.sample(range(1, sides + 1), 3)
         print (dicea)
-        diced = random.sample(range(1, sides +1), 2)
+        diced = random.sample(range(1, sides + 1), 2)
         print (diced)
-        if max(dicea) > max(diced) and min(dicea) > (diced):
+        if max(dicea) > max(diced) and min(dicea) > min(diced):
             print ("Attacker Wins Both")
-        if max(dicea) > max(diced) and min(dicea) < (diced):
+        if max(dicea) > max(diced) and min(dicea) < min(diced):
             print ("Both Lose 1")
-        if max(dicea) < max(diced) and min(dicea) > (diced):
+        if max(dicea) < max(diced) and min(dicea) > min(diced):
             print ("Both Lose 1")
-        if max(dicea) < max(diced) and min(dicea) < (diced):
+        if max(dicea) < max(diced) and min(dicea) < min(diced):
             print ("Defender Wins Both")
-            
+        if max(dicea) == max(diced) and min(dicea) == min(diced):
+            print ("Reroll")
+        if max(dicea) == max(diced) or min(dicea) == min(diced):
+            print ("Reroll")
